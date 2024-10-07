d006c71b5b131cdc14c6f3bcaa5181792f808c90
gespel <heimbrodt@uni-potsdam.de>
2024-10-07 22:54:40 +0200

    added version

--- main.go
+++ main.go
@@ -19,8 +19,13 @@ func (kh *KubeHaven) Name() string {
 	return kh.name
 }
 
+func (kh *KubeHaven) Version() string {
+	return kh.version
+}
+
 func main() {
 	fmt.Println("asd")
 	kh := newKubeHaven()
 	fmt.Println(kh.Name())
+	fmt.Println(kh.Version())
 }
